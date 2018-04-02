be1d1d36426ecb42147326baa12625f5076deb0f
karuta <meta7p.takapon@gmail.com>
2018-04-03 07:28:04 +0900

    画像の開閉機能を追加

--- src/renderer/CompareView/ImageViewer.js
+++ src/renderer/CompareView/ImageViewer.js
@@ -22,33 +22,119 @@ export default class ImageViewer extends Component {
     if (position === 'diff') {
       this.setState({isExpandDiff:!isExpandDiff})
     }
-
-    this.setState({isExpandCompare:!isExpandCompare});
+    else {
+      this.setState({isExpandCompare:!isExpandCompare});
+    }
 
   }
+
+  getImageViewerDiffClass() {
+    const {isExpandDiff, isExpandCompare} = this.state;
+
+    if ( isExpandDiff === true && isExpandCompare === true ) {
+      return 'image-viewer--diff';
+    }
+
+    if( isExpandDiff === false && isExpandCompare === true ) {
+      return 'image-viewer--diff--FT';
+    }
+
+    if ( isExpandDiff === true && isExpandCompare === false ) {
+      return 'image-viewer--diff--TF';
+    }
+
+    if( isExpandDiff === false && isExpandCompare === false ) {
+      return 'image-viewer--diff--FF';
+    }
+
+  }
+
+  getImgViewDiffClass() {
+    const {isExpandDiff, isExpandCompare} = this.state;
+
+    if ( isExpandDiff === true && isExpandCompare === true ) {
+      return 'imageView diff'
+    }
+
+    if( isExpandDiff === false && isExpandCompare === true ) {
+      return 'imageView diff--FT'
+    }
+
+    if ( isExpandDiff === true && isExpandCompare === false ) {
+      return 'imageView diff--TF'
+    }
+
+    if( isExpandDiff === false && isExpandCompare === false ) {
+      return 'imageView diff--FF'
+    }
+
+  }
+
+  getImageViewerCompareClass() {
+    const {isExpandDiff, isExpandCompare} = this.state;
+
+    if ( isExpandDiff === true && isExpandCompare === true ) {
+      return 'image-viewer--compare';
+    }
+
+    if( isExpandDiff === false && isExpandCompare === true ) {
+      return 'image-viewer--compare--FT';
+    }
+
+    if ( isExpandDiff === true && isExpandCompare === false ) {
+      return 'image-viewer--compare--TF';
+    }
+
+    if( isExpandDiff === false && isExpandCompare === false ) {
+      return 'image-viewer--compare--FF';
+    }
+
+  }
+
+  getImageViewerCompareImgsClass() {
+    const {isExpandDiff, isExpandCompare} = this.state;
+
+    if ( isExpandDiff === true && isExpandCompare === true ) {
+      return 'image-viewer--compare-imgs';
+    }
+
+    if( isExpandDiff === false && isExpandCompare === true ) {
+      return 'image-viewer--compare-imgs--FT';
+    }
+
+    if ( isExpandDiff === true && isExpandCompare === false ) {
+      return 'image-viewer--compare-imgs--TF';
+    }
+
+    if( isExpandDiff === false && isExpandCompare === false ) {
+      return 'image-viewer--compare-imgs--FF';
+    }
+
+  }
+
 
   render() {
     const {diff,before,after} = this.props;
     const {isExpandDiff, isExpandCompare} = this.state;
     return (
       <div id="image-viewer">
-        <div class="image-viewer--diff">
+        <div class={this.getImageViewerDiffClass()}>
           <div class='image-viewer-expander'>
             <IconButton aria-label="ExpandLess" onClick={() => {this.toggleExpand('diff')}}>
               { isExpandDiff ? <ExpandLess /> : <ExpandMore /> }
             </IconButton>
           </div>
-          <ImgView title='diff' imgSrc={diff} />
+          <ImgView title='diff' imgSrc={diff} className={this.getImgViewDiffClass()} />
         </div>
-        <div class="image-viewer--compare">
+        <div class={this.getImageViewerCompareClass()}>
           <div class='image-viewer-expander'>
             <IconButton aria-label="ExpandLess" onClick={() => {this.toggleExpand('compare')}}>
               { isExpandCompare ? <ExpandLess /> : <ExpandMore /> }
             </IconButton>
           </div>
-          <div class='image-viewer--compare-imgs'>
-            <ImgView title='left' imgSrc={before}/>
-            <ImgView title='right' imgSrc={after}/>
+          <div class={this.getImageViewerCompareImgsClass()}>
+            <ImgView title='title' imgSrc={before} className='left' />
+            <ImgView title='title' imgSrc={after} className='right' />
           </div>
         </div>
       </div>
--- src/renderer/CompareView/ImgView.js
+++ src/renderer/CompareView/ImgView.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import styles from '../../css/app.css';
 
-export default function ({title, imgSrc, history}) {
+export default function ({title, className, imgSrc}) {
   return (
-    <div className={'imgView' + ' ' + title}>
+    <div className={'imgView' + ' ' + className}>
       <div className='imgView__title'>{title}</div>
       <img className='imgView__img' src={imgSrc}/>
-      <button className='imgView__btn' onClick={ () => {console.log(history); history.push('/') }}>Button</button>
     </div>
   );
 }
