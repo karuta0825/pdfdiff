140e96ddb4bab2ebb506e1c32edf01d053af3147
karuta0825 <karuta0825@users.noreply.github.com>
2018-04-03 20:37:40 +0900

    stateを上位に移行

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -8,14 +8,36 @@ import { getRootHtmlPath } from '../utils/Path';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      leftPath : '',
+      rightPath : ''
+    }
+    this.setPath = this.setPath.bind(this);
+  }
+
+  setPath(path, place) {
+    const {leftPath, rightPath} = this.state;
+
+    if (place === 'right') {
+      this.setState({leftPath:leftPath, rightPath:path})
+    }
+
+    if (place === 'left') {
+      this.setState({leftPath:path, rightPath:rightPath})
+    }
+  }
+
   render() {
+    const {leftPath, rightPath} = this.state;
     return (
       <MuiThemeProvider>
         <BrowserRouter>
         <div id='router'>
-          <Route exact path={getRootHtmlPath()} component={OverLook} />
+          <Route exact path={getRootHtmlPath()} render={(props)=> <Loader setPath={this.setPath} rightPath={rightPath} leftPath={leftPath} history={props.history}/>} />
           <Route exact path="/" component={Loader} />
-          <Route path="/memo" component={OverLook} />
+          <Route path="/memo" render={() => <OverLook leftPath={leftPath} rightPath={rightPath} />}/>
         </div>
         </BrowserRouter>
       </MuiThemeProvider>
